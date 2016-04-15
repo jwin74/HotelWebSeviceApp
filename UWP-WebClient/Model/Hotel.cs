49e47565d53d88a05a591b3b985a49884e408003
jwin74 <jasp0359@edu.easj.dk>
2016-04-15 14:56:09 +0200

    Hotel Model Class Added

---
+++ UWP-WebClient/Model/Hotel.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+
+namespace AdvWebApi_UWP_App.model
+
+{
+    public partial class Hotel
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Hotel()
+        {
+            Rooms = new HashSet<Room>();
+        }
+
+        public int Hotel_No { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Room> Rooms { get; set; }
+    }
+}
