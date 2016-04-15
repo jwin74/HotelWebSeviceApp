840a9f7c71520f2e4b898996b42afc068f749b16
jwin74 <jasp0359@edu.easj.dk>
2016-04-15 14:58:47 +0200

    GuestCatalogSingleton Model Class Added

---
+++ UWP-WebClient/Model/GuestCatalogSingleton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+
+namespace AdvWebApi_UWP_App.model
+{
+    public class GuestCatalogSingleton
+    {
+        private static GuestCatalogSingleton _instance;
+
+        private GuestCatalogSingleton()
+        {
+            Guests=new ObservableCollection<Guest>();
+        }
+
+        public ObservableCollection<Guest> Guests { get; set; }
+
+        public static GuestCatalogSingleton Instance
+        {
+            // ReSharper disable once ConvertPropertyToExpressionBody
+            get { return _instance ?? (_instance = new GuestCatalogSingleton()); }
+        }
+
+        public void Add(Guest newGuest)
+        {
+            Guests.Add(newGuest);
+        }
+
+        public void Remove(Guest guestToBeRemoved)
+        {
+            Guests.Remove(guestToBeRemoved);
+        }
+    }
+}
